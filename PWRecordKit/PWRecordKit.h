511dc2cfe6f46683676ea830efafa6439b9dd2a0
jinwoong <baseball0020@gmail.com>
2023-11-15 12:19:32 +0900

    Initial Commit

---
+++ PWRecordKit/PWRecordKit.h
@@ -0,0 +1,18 @@
+//
+//  PWRecordKit.h
+//  PWRecordKit
+//
+//  Created by jinwoong Kim on 11/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PWRecordKit.
+FOUNDATION_EXPORT double PWRecordKitVersionNumber;
+
+//! Project version string for PWRecordKit.
+FOUNDATION_EXPORT const unsigned char PWRecordKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PWRecordKit/PublicHeader.h>
+
+
