4ad6c6f676a176eaa660b368ef9659cad181ddb6
jinwoong <baseball0020@gmail.com>
2023-11-18 19:12:30 +0900

    feat: implement DefaultAudioRecorder

--- PWRecordKit/Record/AudioRecorder/DefaultAudioRecorder.swift
+++ PWRecordKit/Record/AudioRecorder/DefaultAudioRecorder.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import AVFAudio
+
+enum AudioError: Error {
+    case permissionDenied
+}
+
+public actor DefaultAudioRecorder: AudioRecorder {
+    var recorder: AVAudioRecorder?
+    
+    public init() {}
+    
+    public var currentTime: TimeInterval? {
+        guard let recorder = self.recorder,
+              recorder.isRecording else {
+            return nil
+        }
+        return recorder.currentTime
+    }
+    
+    public func requestPermission() async -> Bool {
+        if #available(iOS 17, *) {
+            return await AVAudioApplication.requestRecordPermission()
+        } else {
+            return await withUnsafeContinuation { continuation in
+                AVAudioSession.sharedInstance().requestRecordPermission { permission in
+                    continuation.resume(returning: permission)
+                }
+            }
+        }
+    }
+    
+    public func start(url: URL) async throws {
+        guard await requestPermission() else {
+            throw AudioError.permissionDenied
+        }
+        
+        let recorder = try AVAudioRecorder(
+            url: url,
+            settings: [
+                AVFormatIDKey: kAudioFormatMPEG4AAC,
+                AVSampleRateKey: 44100,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+        )
+        self.recorder = recorder
+        try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default)
+        try AVAudioSession.sharedInstance().setActive(true)
+        
+        self.recorder?.record()
+    }
+    
+    public func stop() {
+        self.recorder?.stop()
+        self.recorder = nil
+        try? AVAudioSession.sharedInstance().setActive(false)
+    }
+}
